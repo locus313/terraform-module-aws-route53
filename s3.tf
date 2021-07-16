735c0975da5af7dfdaf16a6c39627bd73d800979
Patrick Lewis <patrick.lewis@modusbox.com>
2021-07-16 13:25:08 -0700

    fix(TF): add tfsec ignore (#16)

--- s3.tf
+++ s3.tf
@@ -1,4 +1,4 @@
-#tfsec:ignore:AWS002 tfsec:ignore:AWS017 tfsec:ignore:AWS077
+#tfsec:ignore:AWS002 tfsec:ignore:AWS017 tfsec:ignore:AWS077 tfsec:ignore:AWS098
 resource "aws_s3_bucket" "records_wr" {
   for_each   = var.records_wr
   bucket     = each.key
