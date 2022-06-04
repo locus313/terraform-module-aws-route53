20d8afa5eedf9e41b8428a98ca5a2f094a155f7a
Patrick Lewis <pat@lo5t.com>
2022-06-04 08:03:21 -0700

    fix(S3): aws provider update (#19)

--- s3.tf
+++ s3.tf
@@ -8,5 +8,7 @@ resource "aws_s3_bucket_website_configuration" "records_wr" {
   for_each   = var.records_wr
   bucket = aws_s3_bucket.records_wr[each.key].id
 
-  redirect_all_requests_to = each.value
+  redirect_all_requests_to {
+    host_name = each.value
+  }
 }
