1ec3694cc5f25d3aac0c89ee4509b101fa36c02e
Patrick Lewis <4015312+locus313@users.noreply.github.com>
2024-06-22 08:45:19 +0000

    updates

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "this_route53_zone_zone_id" {
   description = "Zone ID of Route53 zone"
-  value       = aws_route53_zone.this.zone_id
+  value       = aws_route53_zone.this[0].zone_id
 }
 
 output "this_route53_zone_name_servers" {
   description = "Name servers of Route53 zone"
-  value       = aws_route53_zone.this.name_servers
+  value       = aws_route53_zone.this[0].name_servers
 }
