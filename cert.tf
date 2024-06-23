dd5da74ce108f7f79096c340da1ea5fc18f67d35
Patrick Lewis <4015312+locus313@users.noreply.github.com>
2024-06-23 02:27:44 +0000

    update

--- cert.tf
+++ cert.tf
@@ -4,6 +4,8 @@ resource "aws_acm_certificate" "records_wr" {
   provider          = aws.acm
   domain_name       = each.key
   validation_method = "DNS"
+
+  depends_on = [aws_route53_zone.this]
 }
 
 resource "aws_acm_certificate_validation" "records_wr" {
@@ -12,4 +14,6 @@ resource "aws_acm_certificate_validation" "records_wr" {
   provider                = aws.acm
   certificate_arn         = aws_acm_certificate.records_wr[each.key].arn
   validation_record_fqdns = [for record in aws_route53_record.records_wr_validation : record.fqdn]
+
+  depends_on = [aws_route53_zone.this]
 }
