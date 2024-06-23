9afb230720c958750201215beffd7768040ccf5b
Patrick Lewis <4015312+locus313@users.noreply.github.com>
2024-06-22 21:21:12 -0700

    refactor example and unit tests (#10)
    * Update version for example
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update

--- example/main.tf
+++ example/main.tf
@@ -1,46 +1,45 @@
-module "example-com" {
+module "lo5t-dev" {
    source = "locus313/aws-route53/module"
-   version = "1.0.4"
+   version = "1.0.5"
    
    enabled                                                     = true
-   primary_domain                                              = "example.com"
+   primary_domain                                              = "lo5t.dev"
    
    records_wr = {
-     "support.example.com"                                     = "https://example.atlassian.net/servicedesk/customer/portals"
    }
    
    records_a = {
-     "example.com"                                             = ["12.23.45.56"]
+     "lo5t.dev"                                             = ["12.23.45.56"]
    }
 
    records_aaaa = {
-     "example.com"                                             = ["::ffff:c17:2d38"]
+     "lo5t.dev"                                             = ["::ffff:c17:2d38"]
    }
 
    records_caa = {
-     "example.com"                                             = ["0 issue \"letsencrypt.org\""]
+     "lo5t.dev"                                             = ["0 issue \"letsencrypt.org\""]
    }
    
    records_cname = {
-     "calendar.example.com"                                    = ["ghs.googlehosted.com"]
-     "docs.example.com"                                        = ["ghs.googlehosted.com"]
-     "ftp.example.com"                                         = ["example.com"]
-     "mail.example.com"                                        = ["ghs.googlehosted.com"]
-     "sites.example.com"                                       = ["ghs.googlehosted.com"]
-     "www.example.com"                                         = ["example.com"]
+     "calendar.lo5t.dev"                                    = ["ghs.googlehosted.com"]
+     "docs.lo5t.dev"                                        = ["ghs.googlehosted.com"]
+     "ftp.lo5t.dev"                                         = ["lo5t.dev"]
+     "mail.lo5t.dev"                                        = ["ghs.googlehosted.com"]
+     "sites.lo5t.dev"                                       = ["ghs.googlehosted.com"]
+     "www.lo5t.dev"                                         = ["lo5t.dev"]
    }
    
    records_mx = {
-     "example.com"                                             = ["10 aspmx.l.google.com","20 alt1.aspmx.l.google.com","30 alt2.aspmx.l.google.com","40 aspmx2.googlemail.com","50 aspmx3.googlemail.com"]
+     "lo5t.dev"                                             = ["10 aspmx.l.google.com","20 alt1.aspmx.l.google.com","30 alt2.aspmx.l.google.com","40 aspmx2.googlemail.com","50 aspmx3.googlemail.com"]
    }
    
    records_txt = {
-     "example.com"                                             = ["v=spf1 include:_spf.google.com ~all"]
-     "google._domainkey.example.com"                           = ["v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA3gtOkCHXv+kDJBpIkr1lq1Ywd4B8FJGPceSv9s7yhUtCk8pKwifLmSKWNEyOvuK5oxIms+4Vc9Pu46bi/wehi5zJynzhkOrzYXdOX6+m4Nb8NbFWr0zZqsM+pDGmYzRjeczY/+txXnOegcbxL+967bIRisnlH2CRD91h1t0NJwsvvN23T5MAMaeJr726piDdE\"\"C6P2nF1apYbGXp0DZGz/RvtpCGjASjlpejA8I/xLclZOBn4Ir9pk8gajRSG48D21UKJ3d+PFzYEj9X5n1p1i2trjCqkdyCzU+f3vTUxma5F7fQncKYIeRJwVNbmR03IYfGuicCu13hnVP36aT5yuQIDAQAB"]
+     "lo5t.dev"                                             = ["v=spf1 include:_spf.google.com ~all"]
+     "google._domainkey.lo5t.dev"                           = ["v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA3gtOkCHXv+kDJBpIkr1lq1Ywd4B8FJGPceSv9s7yhUtCk8pKwifLmSKWNEyOvuK5oxIms+4Vc9Pu46bi/wehi5zJynzhkOrzYXdOX6+m4Nb8NbFWr0zZqsM+pDGmYzRjeczY/+txXnOegcbxL+967bIRisnlH2CRD91h1t0NJwsvvN23T5MAMaeJr726piDdE\"\"C6P2nF1apYbGXp0DZGz/RvtpCGjASjlpejA8I/xLclZOBn4Ir9pk8gajRSG48D21UKJ3d+PFzYEj9X5n1p1i2trjCqkdyCzU+f3vTUxma5F7fQncKYIeRJwVNbmR03IYfGuicCu13hnVP36aT5yuQIDAQAB"]
    }
    
    records_ns = {
-     "clusters.example.com"                                    = ["ns-128.awsdns-16.com","ns-1533.awsdns-63.org","ns-1554.awsdns-02.co.uk","ns-956.awsdns-55.net"]
-     "envs.example.com"                                        = ["ns-1103.awsdns-09.org","ns-1587.awsdns-06.co.uk","ns-378.awsdns-47.com","ns-693.awsdns-22.net"]
+     "clusters.lo5t.dev"                                    = ["ns-128.awsdns-16.com","ns-1533.awsdns-63.org","ns-1554.awsdns-02.co.uk","ns-956.awsdns-55.net"]
+     "envs.lo5t.dev"                                        = ["ns-1103.awsdns-09.org","ns-1587.awsdns-06.co.uk","ns-378.awsdns-47.com","ns-693.awsdns-22.net"]
    }
 }
--- example/outputs.tf
+++ example/outputs.tf
@@ -1,9 +1,9 @@
 output "zone_id" {
   description = "Zone ID of Route53 zone"
-  value       = module.example-com[0].this_route53_zone_zone_id
+  value       = module.lo5t-dev.this_route53_zone_zone_id
 }
 
 output "name_servers" {
   description = "Name servers of Route53 zone"
-  value       = module.example-com[0].this_route53_zone_name_servers
+  value       = module.lo5t-dev.this_route53_zone_name_servers
 }
